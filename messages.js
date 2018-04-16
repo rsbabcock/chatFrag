493140277032c778dfaa7d0312d2afa68b394b5e
rsbabcock <rachael.s.babcock@gmail.com>
2018-04-16 13:55:25 -0500

    added fragmentation and is working

--- messages.js
+++ messages.js
@@ -1,17 +1,20 @@
 // In your JavaScript, use querySelector() to obtain a reference to that article DOM element.
 
-const articleRef = document.querySelector('#messages')
+let articleFragment = document.createDocumentFragment()
+// const articleRef = document.querySelector('#messages')
 // Create five (5) section elements, each with a class of message, and with textContent of your choosing.
 const createChat = () => {
     // Using appendChild(), attach each message as a child to the messages element.
     const messages = ["Hi", "You're a crazy peson!", "My name is Johann Schmitt", "Bach is my favourite", "I would like to meet you"]    
-    const sectionElement = document.createElement('section')
-    messages.forEach(currentMessage => {
-        sectionElement.textContent = currentMessage
-        sectionElement.appendChild(currentMessage)
-        articleRef.appendChild(sectionElement)
-        
-        
-        
-    })
+    for (let i = 0; i < messages.length; i ++) {
+        const sectionElement = document.createElement('section')
+        sectionElement.className = 'message'   
+        sectionElement.textContent += messages[i]
+        articleFragment.appendChild(sectionElement)
+          
+    }
 }
+
+createChat()
+
+document.querySelector('#messages').appendChild(articleFragment)
