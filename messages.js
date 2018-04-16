d9a57a6e2a936232ea9d62081c551c97912cecea
rsbabcock <rachael.s.babcock@gmail.com>
2018-04-16 13:20:29 -0500

    added files and first pass of chat function

---
+++ messages.js
@@ -0,0 +1,17 @@
+// In your JavaScript, use querySelector() to obtain a reference to that article DOM element.
+
+const articleRef = document.querySelector('#messages')
+// Create five (5) section elements, each with a class of message, and with textContent of your choosing.
+const createChat = () => {
+    // Using appendChild(), attach each message as a child to the messages element.
+    const messages = ["Hi", "You're a crazy peson!", "My name is Johann Schmitt", "Bach is my favourite", "I would like to meet you"]    
+    const sectionElement = document.createElement('section')
+    messages.forEach(currentMessage => {
+        sectionElement.textContent = currentMessage
+        sectionElement.appendChild(currentMessage)
+        articleRef.appendChild(sectionElement)
+        
+        
+        
+    }
+}
